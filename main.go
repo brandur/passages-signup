994ad05aa56905bdee217301fe32d76140d2b7ca
Brandur <brandur@brandur.org>
2020-04-07 15:38:14 -0700

    Revert rate increases
    It turns out that rate limiting problems were not my app, but coming
    from within Google Cloud Run itself. Revert rates back to the original
    levels.

--- main.go
+++ main.go
@@ -321,8 +321,8 @@ func getRateLimiter() (*throttled.HTTPRateLimiter, error) {
 	}
 
 	quota := throttled.RateQuota{
-		MaxBurst: 30,
-		MaxRate:  throttled.PerSec(10),
+		MaxBurst: 20,
+		MaxRate:  throttled.PerSec(5),
 	}
 
 	rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
