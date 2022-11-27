brandur/passages-signup

commit 4b978bd5291b706654890b7992908e89133da854

Add maintenance mode middleware to facilitate database migration (#23)
I need to move these databases off of Heroku, so add a maintenance
mode middleware to help shut down the app while that's in progress.

main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/brandur/passages-signup/command"
 	"github.com/brandur/passages-signup/db"
 	"github.com/brandur/passages-signup/mailclient"
+	"github.com/brandur/passages-signup/middleware"
 	"github.com/brandur/passages-signup/newslettermeta"
 	"github.com/brandur/passages-signup/ptemplate"
 )
@@ -55,6 +56,12 @@ type Conf struct {
 
 	// MailgunAPIKey is a key for Mailgun used to send email.
 	MailgunAPIKey string `env:"MAILGUN_API_KEY,required" validate:"required"`
+
+	// MaintenanceMode activates "maintenance mode" in which the service will be
+	// unavailable until maintenance mode has been turned back off again. This
+	// is intended for use for the very most invasive operational work, like if
+	// the main database needs to be migrated to another provider.
+	MaintenanceMode bool `env:"MAINTENANCE_MODE"`
 
 	// Newsletter is the newsletter to send. Should be either `nanoglyph` or
 	// `passages` and defaults to the latter. Along with one of the available
@@ -170,19 +177,27 @@ func NewServer(ctx context.Context, conf *Conf) (*Server, error) {
 	}
 
 	r := mux.NewRouter()
+
+	// Keep static assets on a clean router so that they can be served even in
+	// maintenance mode.
+	//
+	// In production serves assets that have been slurped up with go:embed. In
+	// other environments, reads directly from disk for reasy reloading.
+	r.PathPrefix("/public/").Handler(staticAssetsHandler(conf.isProduction()))
+
+	r = r.NewRoute().Subrouter()
+	r.Use(middleware.NewMaintenanceModeMiddleware(conf.MaintenanceMode, renderer).Wrapper)
+
 	r.HandleFunc("/", s.handleShow)
 	r.HandleFunc("/confirm/{token}", s.handleConfirm)
 	r.HandleFunc("/submit", s.handleSubmit)
 
 	// Easy message previews for development.
 	if !conf.isProduction() {
-		r.HandleFunc("/messages/confirm", s.handleShowConfirmMessagePreview)
-		r.HandleFunc("/messages/confirm_plain", s.handleShowConfirmMessagePlainPreview)
+		r.HandleFunc("/dev/messages/confirm", s.handleShowConfirmMessagePreview)
+		r.HandleFunc("/dev/messages/confirm_plain", s.handleShowConfirmMessagePlainPreview)
+		r.HandleFunc("/dev/maintenance", s.handleShowMaintenance)
 	}
-
-	// In production serves assets that have been slurped up with go:embed. In
-	// other environments, reads directly from disk for reasy reloading.
-	r.PathPrefix("/public/").Handler(staticAssetsHandler(conf.isProduction()))
 
 	s.handler = r
 
@@ -285,6 +300,12 @@ func (s *Server) handleShowConfirmMessagePlainPreview(w http.ResponseWriter, r *
 		return s.renderer.RenderTemplate(w, "views/messages/confirm_plain", map[string]interface{}{
 			"token": "bc492bd9-2aea-458a-aea1-cd7861c334d1",
 		})
+	})
+}
+
+func (s *Server) handleShowMaintenance(w http.ResponseWriter, r *http.Request) {
+	s.withErrorHandling(w, func() error {
+		return s.renderer.RenderTemplate(w, "views/maintenance", map[string]interface{}{})
 	})
 }
 

.envrc.sample
@@ -1,6 +1,7 @@
 export DATABASE_URL=postgres://localhost/passages-signup?sslmode=disable
 export ENABLE_RATE_LIMITER=true
 export MAILGUN_API_KEY=
+#export MAINTENANCE_MODE=true
 export NEWSLETTER_ID=passages
 export PASSAGES_ENV=testing
 export PORT=

middleware/maintenance_mode.go
@@ -1,0 +1,42 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/brandur/passages-signup/ptemplate"
+)
+
+// MaintenanceModeMiddleware sits above the server stack and allows the entire
+// service to be put into "maintenance mode" in which all API requests are
+// termined with a 503 Service Unavailable. This in turn allows us to carry out
+// critical maintenance on core infrastructure like the database without having
+// to worry about load or writes.
+type MaintenanceModeMiddleware struct {
+	maintenanceMode bool
+	renderer        *ptemplate.Renderer
+}
+
+func NewMaintenanceModeMiddleware(maintenanceMode bool, renderer *ptemplate.Renderer) *MaintenanceModeMiddleware {
+	return &MaintenanceModeMiddleware{
+		maintenanceMode: maintenanceMode,
+		renderer:        renderer,
+	}
+}
+
+func (m *MaintenanceModeMiddleware) Wrapper(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if m.maintenanceMode {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			if err := m.renderer.RenderTemplate(w, "views/maintenance", map[string]interface{}{}); err != nil {
+				logrus.Errorf("Error rendering maintenance mode: %v", err)
+				_, _ = w.Write([]byte(fmt.Sprintf("Error rendering maintenance mode: %v", err)))
+			}
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}

middleware/maintenance_mode_test.go
@@ -1,0 +1,76 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/brandur/passages-signup/newslettermeta"
+	"github.com/brandur/passages-signup/ptemplate"
+)
+
+func TestMaintenanceModeMiddlewareWrapper(t *testing.T) {
+	var (
+		handler  http.Handler
+		renderer *ptemplate.Renderer
+	)
+
+	setup := func(test func(*testing.T)) func(*testing.T) {
+		return func(t *testing.T) {
+			t.Helper()
+
+			handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write([]byte("ok."))
+			})
+
+			var err error
+			renderer, err = ptemplate.NewRenderer(&ptemplate.RendererConfig{
+				DynamicReload:  true,
+				NewsletterMeta: newslettermeta.MustMetaFor("list.brandur.org", newslettermeta.PassagesID),
+				PublicURL:      "https://example.com",
+				Templates:      os.DirFS("../"),
+			})
+			require.NoError(t, err)
+
+			test(t)
+		}
+	}
+
+	t.Run("MaintenanceOn", setup(func(t *testing.T) { //nolint:thelper
+		handler = NewMaintenanceModeMiddleware(true, renderer).Wrapper(handler)
+
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "https://example.com", nil)
+		handler.ServeHTTP(recorder, req)
+
+		res := recorder.Result() //nolint:bodyclose
+		require.Equal(t, http.StatusServiceUnavailable, res.StatusCode)
+
+		data := recorder.Body.Bytes()
+		require.Contains(t, string(data), "This application is currently in maintenance mode")
+	}))
+
+	t.Run("MaintenanceOff", setup(func(t *testing.T) { //nolint:thelper
+		handler = NewMaintenanceModeMiddleware(false, renderer).Wrapper(handler)
+
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "https://example.com", nil)
+		handler.ServeHTTP(recorder, req)
+
+		requireStatusOrPrintBody(t, http.StatusOK, recorder)
+	}))
+}
+
+func requireStatusOrPrintBody(t *testing.T, expectedStatusCode int, recorder *httptest.ResponseRecorder) {
+	t.Helper()
+	//nolint:bodyclose
+	require.Equal(t, expectedStatusCode, recorder.Result().StatusCode,
+		"Expected status %v, but got %v; body was: %s",
+		expectedStatusCode,
+		recorder.Result().StatusCode,
+		recorder.Body.String(),
+	)
+}

views/maintenance.ace
@@ -1,0 +1,3 @@
+= content main
+  #passages {{.NewsletterMeta.Name}}
+  | This application is currently in maintenance mode to facilitate non-standard operations. Please retry this request shortly.

commit c0588dc754e0ccf5d9c1c5efe87d9070c330f6e6

Fix rendering of static assets + add test
Fixes rendering of static assets that regressed in #23 by making sure
our inner router isn't mounted as the main one. Add a test for static
asset requests to make this a bit safer in the future.

go.mod
@@ -24,11 +24,13 @@ require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-chi/chi v4.1.2+incompatible // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/gorilla/css v1.0.0 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect

go.sum
@@ -23,6 +23,9 @@ github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 h1:JWuenKqqX8nojt
 github.com/facebookgo/stack v0.0.0-20160209184415-751773369052/go.mod h1:UbMTZqLaRiH3MsBH8va0n7s1pQYcu3uTb8G4tygF4Zg=
 github.com/facebookgo/subset v0.0.0-20150612182917-8dac2c3c4870 h1:E2s37DuLxFhQDg5gKsWoLBOB0n+ZW8s599zru8FJ2/Y=
 github.com/facebookgo/subset v0.0.0-20150612182917-8dac2c3c4870/go.mod h1:5tD+neXqOorC30/tWg0LCSkrqj/AR6gu8yY8/fpw1q0=
+github.com/felixge/httpsnoop v1.0.1/go.mod h1:m8KPJKqk1gH5J9DgRY2ASl2lWCfGKXixSwevea8zH2U=
+github.com/felixge/httpsnoop v1.0.3 h1:s/nj+GCswXYzN5v2DpNMuMQYe+0DDwt5WVCU6CWBdXk=
+github.com/felixge/httpsnoop v1.0.3/go.mod h1:m8KPJKqk1gH5J9DgRY2ASl2lWCfGKXixSwevea8zH2U=
 github.com/go-chi/chi v4.0.0+incompatible/go.mod h1:eB3wogJHnLi3x/kFX2A+IbTBlXxmMeXJVKy9tTv1XzQ=
 github.com/go-chi/chi v4.1.2+incompatible h1:fGFk2Gmi/YKXk0OmGfBh0WgmN3XB8lVnEyNz34tQRec=
 github.com/go-chi/chi v4.1.2+incompatible/go.mod h1:eB3wogJHnLi3x/kFX2A+IbTBlXxmMeXJVKy9tTv1XzQ=
@@ -46,6 +49,8 @@ github.com/google/uuid v1.3.0 h1:t6JiXgmwXMjEs8VusXIJk2BXHsn+wx8BZdTaoZ5fu7I=
 github.com/google/uuid v1.3.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
 github.com/gorilla/css v1.0.0 h1:BQqNyPTi50JCFMTw/b67hByjMVXZRwGha6wxVGkeihY=
 github.com/gorilla/css v1.0.0/go.mod h1:Dn721qIggHpt4+EFCcTLTU/vk5ySda2ReITrtgBl60c=
+github.com/gorilla/handlers v1.5.1 h1:9lRY6j8DEeeBT10CvO9hGW0gmky0BprnvDI5vfhUHH4=
+github.com/gorilla/handlers v1.5.1/go.mod h1:t8XrUpc4KVXb7HGyJ4/cEnwQiaxrX/hz1Zv/4g96P1Q=
 github.com/gorilla/mux v1.8.0 h1:i40aqfkR1h2SlN9hojwV5ZA91wcXFOvkdNIeFDP5koI=
 github.com/gorilla/mux v1.8.0/go.mod h1:DVbg23sWSpFRCP0SfiEN6jmj59UnW/n46BH5rLB71So=
 github.com/hashicorp/golang-lru v0.5.4 h1:YDjusn29QI/Das2iO9M0BHnIbxPeyuCHsjMW+lJfyTc=

main.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/jackc/pgx/v4"
 	"github.com/joeshaw/envdecode"
@@ -185,18 +186,18 @@ func NewServer(ctx context.Context, conf *Conf) (*Server, error) {
 	// other environments, reads directly from disk for reasy reloading.
 	r.PathPrefix("/public/").Handler(staticAssetsHandler(conf.isProduction()))
 
-	r = r.NewRoute().Subrouter()
-	r.Use(middleware.NewMaintenanceModeMiddleware(conf.MaintenanceMode, renderer).Wrapper)
+	innerRouter := r.NewRoute().Subrouter()
+	innerRouter.Use(middleware.NewMaintenanceModeMiddleware(conf.MaintenanceMode, renderer).Wrapper)
 
-	r.HandleFunc("/", s.handleShow)
-	r.HandleFunc("/confirm/{token}", s.handleConfirm)
-	r.HandleFunc("/submit", s.handleSubmit)
+	innerRouter.HandleFunc("/", s.handleShow)
+	innerRouter.HandleFunc("/confirm/{token}", s.handleConfirm)
+	innerRouter.HandleFunc("/submit", s.handleSubmit)
 
 	// Easy message previews for development.
 	if !conf.isProduction() {
-		r.HandleFunc("/dev/messages/confirm", s.handleShowConfirmMessagePreview)
-		r.HandleFunc("/dev/messages/confirm_plain", s.handleShowConfirmMessagePlainPreview)
-		r.HandleFunc("/dev/maintenance", s.handleShowMaintenance)
+		innerRouter.HandleFunc("/dev/messages/confirm", s.handleShowConfirmMessagePreview)
+		innerRouter.HandleFunc("/dev/messages/confirm_plain", s.handleShowConfirmMessagePlainPreview)
+		innerRouter.HandleFunc("/dev/maintenance", s.handleShowMaintenance)
 	}
 
 	s.handler = r
@@ -447,9 +448,12 @@ func redirectToHTTPS(next http.Handler) http.Handler {
 }
 
 func staticAssetsHandler(useEmbedded bool) http.Handler {
+	var handler http.Handler
 	if useEmbedded {
-		return http.FileServer(http.FS(embeddedAssets))
+		handler = http.FileServer(http.FS(embeddedAssets))
+	} else {
+		handler = http.StripPrefix("/public/", http.FileServer(http.Dir("./public")))
 	}
-
-	return http.StripPrefix("/public/", http.FileServer(http.Dir("./public")))
+	fmt.Printf("adding loggin handler, embedded = %v ...\n", useEmbedded)
+	return handlers.CombinedLoggingHandler(os.Stdout, handler)
 }

main_test.go
@@ -39,6 +39,39 @@ func makeServer(ctx context.Context, t *testing.T, txStarter db.TXStarter, newsl
 }
 
 func TestStaticAssets(t *testing.T) {
+	setup := func(test func(*testing.T)) func(*testing.T) {
+		return func(t *testing.T) {
+			t.Helper()
+			test(t)
+		}
+	}
+
+	// Wraps the handler in a mux router for a more realistic simulation.
+	wrapHandler := func(handler http.Handler) http.Handler {
+		r := mux.NewRouter()
+		r.PathPrefix("/public/").Handler(handler)
+		return r
+	}
+
+	t.Run("Disk", setup(func(t *testing.T) { //nolint:thelper
+		handler := wrapHandler(staticAssetsHandler(false))
+
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/public/tiny-preload-image.png", nil)
+		handler.ServeHTTP(recorder, req)
+
+		requireStatusOrPrintBody(t, http.StatusOK, recorder)
+	}))
+
+	t.Run("Embedded", setup(func(t *testing.T) { //nolint:thelper
+		handler := wrapHandler(staticAssetsHandler(true))
+
+		recorder := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/public/tiny-preload-image.png", nil)
+		handler.ServeHTTP(recorder, req)
+
+		requireStatusOrPrintBody(t, http.StatusOK, recorder)
+	}))
 }
 
 func TestHandleConfirm(t *testing.T) {
@@ -229,4 +262,15 @@ func TestHandleSubmit(t *testing.T) {
 				fmt.Sprintf("Wrong status code (see above); body: %v", string(body)))
 		}))
 	}
+}
+
+func requireStatusOrPrintBody(t *testing.T, expectedStatusCode int, recorder *httptest.ResponseRecorder) {
+	t.Helper()
+	//nolint:bodyclose
+	require.Equal(t, expectedStatusCode, recorder.Result().StatusCode,
+		"Expected status %v, but got %v; body was: %s",
+		expectedStatusCode,
+		recorder.Result().StatusCode,
+		recorder.Body.String(),
+	)
 }